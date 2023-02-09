2758af51adccf2049b8bf6dbe41740f43d23d99f
Akivah <akivahlaviolette@gmail.com>
2023-02-09 10:33:04 -0700

    attempting location tracking

--- Za/ViewController.swift
+++ Za/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import MapKit
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    var currentLocation: CLLocation!
+    let locationManager = CLLocationManager()
+    var pizza: [MKMapItem] = []
+    var initialRegion: MKCoordinateRegion!
+    var isInitialMapLoad: Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        mapView.delegate = self
+        
     }
-
-
-}
-
+    @IBOutlet weak var mapView: MKMapView!
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
+        print(currentLocation)
+    }
+    @IBAction func whenZoomButtonPressed(_ sender: Any) {
+    
+        let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: coordinateSpan)
+        mapView.setRegion(region, animated: true)
+        
+    }
+    
+    
+    }
